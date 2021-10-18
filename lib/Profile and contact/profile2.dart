39a74b7655eaf6f6899f22ac8b0df103a66033e2
Asquare-B <michal.homes11@gmail.com>
2021-10-18 16:00:34 +0530

    18-10/1

--- lib/Profile and contact/profile2.dart
+++ lib/Profile and contact/profile2.dart
@@ -13,6 +13,8 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    var cardStyle = TextStyle( fontSize: 20,fontWeight: FontWeight.w800);
+    var cardStyle1 = TextStyle( fontSize: 16,fontWeight: FontWeight.w600);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -34,7 +36,112 @@ class _ProfileState extends State<Profile> {
           'Education Scheduler', style: TextStyle(color: Colors.black),),
       ),
       drawer: Menu(),
-
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/profile.png',height: 200,)
+              ],
+            ),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Card(
+                      color: Color.fromRGBO(255,165,0,100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Name:',style: cardStyle),
+                            Text('StudentName',style: cardStyle1),
+                          ],
+                        ),
+                      )
+                  ),
+                  Card(
+                      color: Color.fromRGBO(255,165,0,100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Reg.No.:',style: cardStyle),
+                            Text('Registered No',style: cardStyle1),
+                          ],
+                        ),
+                      )
+                  ),
+                  Card(
+                      color: Color.fromRGBO(255,165,0,100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Programme:',style: cardStyle),
+                            Text('Programme name',style: cardStyle1),
+                          ],
+                        ),
+                      )
+                  ),
+                  Card(
+                      color: Color.fromRGBO(255,165,0,100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text("Email:",style: cardStyle),
+                            Text('Student Email',style: cardStyle1),
+                          ],
+                        ),
+                      )
+                  ),
+                  Card(
+                      color: Color.fromRGBO(255,165,0,100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Phone:',style: cardStyle),
+                            Text('Mobile Number',style: cardStyle1),
+                          ],
+                        ),
+                      )
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
