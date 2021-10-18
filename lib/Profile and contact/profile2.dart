0d2dfc9ec2b0c343f54557960008c5f200a0cc16
Asquare-B <michal.homes11@gmail.com>
2021-10-18 15:49:19 +0530

    18-10/1

--- lib/Profile and contact/profile2.dart
+++ lib/Profile and contact/profile2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:software/Drawer/menuDrawer.dart';
+import 'package:flutter/rendering.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -33,46 +34,7 @@ class _ProfileState extends State<Profile> {
           'Education Scheduler', style: TextStyle(color: Colors.black),),
       ),
       drawer: Menu(),
-      body: Stack(
-        children: [
-          SizedBox(
-            height: 115,
-            width: 115,
-            child: Stack(
-              fit: StackFit.expand,
-              clipBehavior: Clip.none,
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage("assets/images/profile.png"),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
 
-class ProfileMenu extends StatelessWidget {
-  const ProfileMenu({
-    Key? key,
-    required this.text,
-  }) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Container(
-        child: Row(
-          children: [
-            Expanded(child: Text(text)),
-          ],
-        ),
-      ),
     );
   }
 }
--- lib/extraClass/extra1.dart
+++ lib/extraClass/extra1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:software/Drawer/menuDrawer.dart';
+import 'package:software/extraClass/slotBooking.dart';
 class Extra1 extends StatefulWidget {
   const Extra1({Key? key}) : super(key: key);
 
@@ -10,6 +11,8 @@ class Extra1 extends StatefulWidget {
 class _Extra1State extends State<Extra1> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var cardStyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -25,16 +28,234 @@ class _Extra1State extends State<Extra1> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30),bottomRight: Radius.circular(30)),
         ),
-        title: Text('Education Scheduler',style: TextStyle(color: Colors.black),),
+        title: Text('Request Portal'),
       ),
       drawer: Menu(),
       body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-
-          ],
-        ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 20,),
+              Center(
+                child: Text(
+                  'Extra Class Scheduling',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    Card(
+                      color: Color.fromRGBO(255,165,0,100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                SizedBox(width: 50,),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Slot',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 22,
+                                      ),
+                                    ),
+                                    SizedBox(height: 15,),
+                                    Text(
+                                      'Week Day',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    SizedBox(height: 5,)
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                SizedBox(height: 35,),
+                                Text(
+                                  'Time',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            MaterialButton(
+                              elevation: 5,
+                              height: 45,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              color: Colors.lightBlueAccent,
+                              onPressed: () {
+                                Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (context)=>SlotBooking()
+                                    )
+                                );
+                              },
+                              child: Text('Book Slot', style: TextStyle(fontSize: 15),),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Color.fromRGBO(255,165,0,100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                SizedBox(width: 50,),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Slot',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 22,
+                                      ),
+                                    ),
+                                    SizedBox(height: 15,),
+                                    Text(
+                                      'Week Day',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    SizedBox(height: 5,)
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                SizedBox(height: 35,),
+                                Text(
+                                  'Time',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            MaterialButton(
+                              elevation: 5,
+                              height: 45,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              color: Colors.lightBlueAccent,
+                              onPressed: () {},
+                              child: Text('Book Slot', style: TextStyle(fontSize: 15),),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Color.fromRGBO(255,165,0,100),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                SizedBox(width: 50,),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Slot',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 22,
+                                      ),
+                                    ),
+                                    SizedBox(height: 15,),
+                                    Text(
+                                      'Week Day',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    SizedBox(height: 5,)
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                SizedBox(height: 35,),
+                                Text(
+                                  'Time',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            MaterialButton(
+                              elevation: 5,
+                              height: 45,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              color: Colors.lightBlueAccent,
+                              onPressed: () {},
+                              child: Text('Book Slot', style: TextStyle(fontSize: 15),),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
       ),
     );
   }
